8c61a4ac4e0ede1fbebb1b4be5ab43d51e09fd8a
Aleksandar <aca.peric93@gmail.com>
2022-09-16 02:02:41 +0200

    bug fixes

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from getpass import getpass
 
-server = SMTP_SSL('smtp.gmail.com', 465)
 
 gmail = input('Your gmail address: ')
 password = getpass('Password: ')
 
-server.login(gmail, password)
+server = SMTP_SSL('smtp.gmail.com', 465)
+try:
+    server.login(gmail, password)
+except:
+    print("Wrong email or password!")
+    exit(1)
+finally:
+    print('Login successful!')
+
 server.ehlo()
 
 msg = MIMEMultipart()
@@ -20,4 +27,11 @@ message = input('MESSAGE: ')
 
 msg.attach(MIMEText(message, 'plain'))
 text = msg.as_string()
-server.sendmail('aca.peric93@gmail.com', 'aleksandar.peric@icloud.com', text)
+
+try:
+    server.sendmail('aca.peric93@gmail.com', 'aleksandar.peric@icloud.com', text)
+except:
+    print('Something went wrong!')
+    exit(1)
+finally:
+    server.quit()
